32e10000b6f878c765bc5aaff97c6dfb6212e1fb
Phil Armstrong <phil.armstrong@yodel.co.uk>
2019-07-24 13:42:06 +0100

    Deleted old database rule file, updated firestore rules

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,11 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 admin.initializeApp();
 
-export const onCreateUser = functions.auth.user()
-	.onCreate(user => {
-		const doc = admin.firestore().collection('/campaigns').doc();
-		doc.set({id: doc.id, name: doc.id, user: user.uid});
-	});
+// export const onCreateUser = functions.auth.user()
+// 	.onCreate(user =>
+// 		admin.firestore()
+// 			.collection('/campaigns')
+// 			.doc(user.uid)
+// 			.set({id: user.uid, name: user.uid, user: user.uid}));
